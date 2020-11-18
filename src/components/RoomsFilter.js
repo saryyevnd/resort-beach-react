02f7c647516261ddd1b55f2316316af754a0d95d
Muhammed <nsaryyev9797@gmail.com>
2020-11-18 17:20:11 +0200

    Complited (RoomsFilter comp) Size filter

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -81,6 +81,31 @@ const RoomsFilter = ({ rooms }) => {
                     />
                 </div>
                 {/* end room price */}
+                {/* {size} */}
+                <div className='form-group'>
+                    <label htmlFor='size'>
+                        room size
+                    </label>
+                    <div className='size-inputs'>
+                        <input 
+                            type='number' 
+                            name='minSize' 
+                            id='size' 
+                            value={minSize}
+                            onChange={handleChange}
+                            className='size-input'
+                        />
+                        <input 
+                            type='number' 
+                            name='maxSize' 
+                            id='size' 
+                            value={maxSize}
+                            onChange={handleChange}
+                            className='size-input'
+                        />
+                    </div>
+                </div>
+                {/* end of size */}
             </form>
         </section>
     )
