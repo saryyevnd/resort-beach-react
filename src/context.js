b8388a5f2672d6c75b3615d32122c1ad3c26fe20
Muhammed <nsaryyev9797@gmail.com>
2020-11-18 18:29:04 +0200

    Component RoomsFilter was completed

--- src/context.js
+++ src/context.js
@@ -58,7 +58,7 @@ class RoomProvider extends Component {
     }
     handleChange = event => {
         const target = event.target
-        const value = event.type === 'checkbox'
+        const value = target.type === 'checkbox'
             ? target.checked : target.value
         const name = event.target.name
         this.setState({
@@ -73,20 +73,30 @@ class RoomProvider extends Component {
         // all the rooms
         let tempRooms = [...rooms];
         // transform value
-        capacity =parseInt(capacity)
+        capacity = parseInt(capacity)
         price = parseInt(price)
         // filter by type
-        if(type !== 'all'){
-            tempRooms = tempRooms.filter(room=>room.type === type)
+        if (type !== 'all') {
+            tempRooms = tempRooms.filter(room => room.type === type)
         }
         // filter by capacity
 
-        if(capacity !== 1){
-            tempRooms = tempRooms.filter(room=>room.capacity >= capacity)
+        if (capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
         }
 
         // filter by price 
-           tempRooms = tempRooms.filter(room=>room.price <= price) 
+        tempRooms = tempRooms.filter(room => room.price <= price)
+        // filter by size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+        // filter by breakfast
+        if(breakfast){
+            tempRooms = tempRooms.filter(room=> room.breakfast === true)
+        }
+        // filter by pets
+        if(pets){
+            tempRooms = tempRooms.filter(room=> room.pets === true)
+        }
         this.setState({
             sortedRooms: tempRooms
         })
